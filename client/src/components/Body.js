2e86f58d6fedc52abb6fd1ce7eb2cd81a791e0ed
Austin Burton <abur3460@gmail.com>
2019-11-09 10:49:53 -0800

    finished most of mvp

---
+++ client/src/components/Body.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Data from './Data'
+
+function Body({ data }) {
+    return (
+        <tbody>
+            {data.map(playerStats => {
+                return <Data playerStats={playerStats} key={playerStats.name}
+                />
+            })}
+        </tbody>
+    )
+}
+
+export default Body
---
+++ client/src/components/Toggle.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components';
+import { useDarkMode } from '../hooks/useDarkMode'
+
+
+const Toggle = () => {
+    const [darkMode, setDarkMode] = useDarkMode(false);
+    const toggleMode = e => {
+        e.preventDefault();
+        setDarkMode(!darkMode);
+    };
+    return (
+        <Button onClick={toggleMode}
+        className={darkMode ? 'toggle toggled' : 'toggle'}
+        >
+        <i class="fas fa-adjust"></i>
+        </Button>
+        
+
+    );
+};
+
+export default Toggle;
+
+const Button = styled.button`
+    width: 2.1rem;
+    height: 2rem;
+    margin: 0 auto;
+    align-items: center;
+    border-radius: 8px;
+    color: black;
+    background-color: darkgrey;
+    border: none;
+    }
+`
